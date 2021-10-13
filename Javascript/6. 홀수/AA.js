a2ab40380ed2138a98b6ee1fd364040a9fdb2459
soonang <nr97819@naver.com>
2021-10-13 23:37:50 +0900

    6. 홀수 해결 (낯선 js 문법)

--- Javascript/6. 홀수/AA.js
+++ Javascript/6. 홀수/AA.js
@@ -1,20 +1,23 @@
 // A, B, C 세 수만을 입력 받는 문제
 function solution(arr){
+    let min=100
+
     let sum=0
-    
-    let min=arr[0]
     for(let i=0;i<arr.length;i++){
         if(arr[i]%2==1){
             sum+=arr[i]
-            
-            if(min<arr[i]){
+            if(min>arr[i]){
                 min=arr[i]
             }
         }
     }
     console.log(sum)
-    console.log(typeof(sum))
+
     return min
 }
 str="12 77 38 41 53 92 85"
-arr=str.split(",")
+arr=str.split(" ")
+for(let i=0;i<arr.length;i++){
+    arr[i]=parseInt(arr[i])
+}
+console.log(solution(arr))
