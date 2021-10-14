343199c06b883dd20cad0e831c7a242ee074e890
soonang <nr97819@naver.com>
2021-10-14 16:57:18 +0900

    9. A를 #으로 풀이

--- Javascript/9. A를 #으로/AA.js
+++ Javascript/9. A를 #으로/AA.js
@@ -1,8 +1,16 @@
-// A, B, C 세 수만을 입력 받는 문제
-function solution(num){
-    let answer
+function solution(text){
+    let answer=''
     
+    let c=''
+    for(let i=0;i<text.length;i++){
+        if(text[i]=='A')
+            answer+=('#')
+        else{
+            answer+=(text[i])
+        }
+    }
     
     return answer
 }
-console.log(solution(178))
+console.log(solution("BANANA"))
+//console.log(solution("APPLICATION")) -> #PPLIC#TION
