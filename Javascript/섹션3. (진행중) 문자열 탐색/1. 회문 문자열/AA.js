94f8059f905533d86fb99e29ac9c90230f8c4769
soonang <nr97819@naver.com>
2021-10-19 08:42:18 +0900

    폴더 정리

---
+++ Javascript/섹션3. (진행중) 문자열 탐색/1. 회문 문자열/AA.js
@@ -0,0 +1,15 @@
+function solution(num, arr){
+
+    let cnt=0
+    
+    
+    return cnt
+}
+let arr=[]
+let n=5
+arr.push([5, 3, 7, 2, 3])
+arr.push([3, 7, 1, 6, 1])
+arr.push([7, 2, 5, 3, 4])
+arr.push([4, 3, 6, 4, 1])
+arr.push([8, 7, 3, 5, 2])
+console.log(solution(n, arr))
