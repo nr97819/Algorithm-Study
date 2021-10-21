049cc53bf01c902869ff60eb23b0e29cd57a4d5f
soonang <nr97819@naver.com>
2021-10-22 00:35:28 +0900

    [js] 1. 회문 문자열 해결

--- Python/섹션 3 - (진행중) 탐색&시뮬레이션(string,1차원,2차원 리스트 탐색)/8. 곶감/AA.py
+++ Python/섹션 3 - (진행중) 탐색&시뮬레이션(string,1차원,2차원 리스트 탐색)/8. 곶감/AA.py
@@ -4,4 +4,7 @@ C:\\_Algorithm-Study-Folder\\Algorithm-Study\\Python\\
     섹션 3 - (진행중) 탐색&시뮬레이션(string,1차원,2차원 리스트 탐색)\\7. 사과나무'''
 sys.stdin=open(currentPath+'\\input.txt', 'rt')
 
+n=int(input())
+arr=[a for _ in list(map(int,input().split()))]
 
+print(arr)
